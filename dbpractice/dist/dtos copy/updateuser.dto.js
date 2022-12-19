26ab087f483fcf374e60cbaa446f9cadf85d75ed
Shoukath Ali shaik <85278231+horusknox@users.noreply.github.com>
2022-12-19 19:32:34 +0530

    deleted the dist folder
    Dist is not supposed to be pushed!!!!!

--- dbpractice/dist/dtos copy/updateuser.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateuserdto = void 0;
-class updateuserdto {
-}
-exports.updateuserdto = updateuserdto;
-//# sourceMappingURL=updateuser.dto.js.map
--- dbpractice/dist/users/dtos/updateuser.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class updateuserdto {
-    username: string;
-    password: string;
-    email: string;
-}
